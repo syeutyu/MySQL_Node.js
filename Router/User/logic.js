94e534e5828df41951acbc183f9c0ecc8d8a0270
origin <syeuty@naver.com>
2017-08-10 17:10:37 +0900

    암호화 방식

--- Router/User/logic.js
+++ Router/User/logic.js
@@ -1,6 +1,16 @@
+let bcrypt = require('bcrypt-nodejs');
+
 exports.signIn = (req, res) => {
-    console.log('signIn')
+    let num = req.query.num;
+    console.log(num);
+    let num1 = 1;
+    console.log(bcrypt.hashSync(num));
+    console.log(bcrypt.hashSync(num1));
+    if (bcrypt.hashSync(num) == bcrypt.hashSync(num1)) {
+        console.log('µ¿µî');
+    }
 };
+
 exports.signUp = (req, res) => {
     console.log('signUp')
 };
